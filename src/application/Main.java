83585de066b2f838bab9aa9a5e4f71b9dbd95787
ummeabeeha.abbas <ummeabeeha.abbas@IC-CS233-27.uc.ucalgary.ca>
2022-07-22 21:51:13 -0600

    Changes type of root to VBox and added a label to the window.

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			VBox root = new VBox();
-			root.getChildren().add(new Label("Abeeha's label"));
+			root.getChildren().add(new Label("Abeehas label"));
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Cheshta's project");
